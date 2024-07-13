bcc044c17831e4e53949302911a8a19bc56aa84c
FelipeAugusto <94065571+FelipeAugustoSilva@users.noreply.github.com>
2024-07-13 06:55:44 -0300

    jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,13 +4,29 @@ async function signup(req, res) {
     try {
         const body = req.body;
 
-        // Correção: Adicione o operador de atribuição (=)
-        const resService = await authService.signup(body);
 
-        res.send(resService);
+        try{
+            // Correção: Adicione o operador de atribuição (=)
+            const resService = await authService.signup(body);
+            return res.status(201).send(resService);
+        } catch (err) {
+            return res.status(409).send(err.message);
+        }
+        
+        
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
 }
 
-export default { signup };
+async function signin(req, res) {
+    const body = req.body;
+
+    try{
+        const token = await authService.signin(body);
+        return res.send(token);
+    } catch (err) {
+        return res.status(401).send(err.message);
+    }
+}
+export default { signup, signin };
